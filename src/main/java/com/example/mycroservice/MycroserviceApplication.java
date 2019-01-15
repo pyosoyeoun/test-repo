93cd30832c1a46d3dad5a949f542b647ecfc6174
t17022@CORP.KOREACB.KR <t17022@CORP.KOREACB.KR>
2019-01-15 14:04:15 +0900

    dfadfasdf:wq

---
+++ src/main/java/com/example/mycroservice/MycroserviceApplication.java
@@ -0,0 +1,14 @@
+package com.example.mycroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycroserviceApplication.class, args);
+	}
+
+}
+

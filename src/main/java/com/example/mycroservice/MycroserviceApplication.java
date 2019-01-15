e741ed3bb606b79cfdedb2db5e79b6689e0faa3f
pyosoyeoun <psy7706041@gmail.com>
2019-01-15 15:02:42 +0900

    tesdddt

--- src/main/java/com/example/mycroservice/MycroserviceApplication.java
+++ src/main/java/com/example/mycroservice/MycroserviceApplication.java
@@ -8,7 +8,7 @@ public class MycroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MycroserviceApplication.class, args);
-		System.out.println("Hello MSA");
+		System.out.println("Hello MSA!!!");
 
 	}
 
